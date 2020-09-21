4d18d9e2c08a45cdcb5381d4c0333ea64514b9e7
Abdelrhman Hisham <abdelrhman.hishamm@outlook.com>
2020-09-22 00:59:26 +0200

    Abdelrhman

--- src/Knn.py
+++ src/Knn.py
@@ -2,32 +2,7 @@ import numpy as np
 import warnings
 from collections import Counter
 
-def KNN2(data , predict , k=3):
-    # if k < len(data):
-    #     warnings.warn("K is less than the number of the classes")
-
-    values = []
-    for classes in data :
-        for features in data[classes]:
-            euclidian_dis = np.linalg.norm(np.array(features) - np.array(predict))
-            # euclidian_dis = np.sum((np.array(features)-np.array(predict))**2)
-            values.append([euclidian_dis,classes])    
-
-    # print("Sorted_Values : ",sorted(values)) 
-    # print("Values : ",values)   
-    result = []
-    for i in sorted(values)[:k] :
-        result.append(i[1])
-
-    print("Resuts : ",result)
-    predected_class = Counter(result).most_common(1) [0] [0]
-
-    return predected_class
-
-
 def KNN(data, labels, predict , k=3):
-    if k < len(data):
-        warnings.warn("K is less than the number of the classes")
 
     values = []
     index = 0
@@ -39,32 +14,24 @@ def KNN(data, labels, predict , k=3):
     result = []
     for i in sorted(values)[:k] :
         result.append(i[1])
-    # print("Resuts : ",result)
     predected_class = Counter(result).most_common(1) [0] [0]
 
     return predected_class
 
 
-# def KNN(data, labels, predict , k=3):
-#     # if k < len(data):
-#     #     warnings.warn("K is less than the number of the classes")
+def Get_Euclidian_Dis(data, labels, predict):
+    if k < len(data):
+        warnings.warn("K is less than the number of the classes !!!!")
 
-#     values = []
-#     index = 0
-#     for folds in range(len(data)) : 
-#         for imges in range(len(data[folds])): 
-#             euclidian_dis = np.linalg.norm(np.array(data[folds][imges]) - np.array(predict))
-#             values.append([euclidian_dis,labels[index]]) 
-#             # print(labels[index])
-#             index +=1   
-#     # print("Sorted_Values : ",sorted(values))
-#     # print("Values : ",values)
-#     result = []
-#     for i in sorted(values)[:k] :
-#         result.append(i[1])
-#     print("Resuts : ",result)
-#     predected_class = Counter(result).most_common(1) [0] [0]
+    values = []
+    index = 0
+    for folds in data : 
+        for imges in folds: 
+            euclidian_dis = np.linalg.norm(np.array(imges) - np.array(predict))
+            values.append([euclidian_dis,labels[index]]) 
+            index +=1   
+    return sorted(values)
 
-#     return predected_class
+
 
 
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,105 +1,68 @@
 from PIL import Image
 import numpy as np
-from Knn import KNN2
 from sklearn.utils import shuffle
-from random import randrange
-# "F:/Handwritten-digit-recognition/Task Dataset/Train/Training Labels.txt"
-# "F:/Handwritten-digit-recognition/Task Dataset/Test/test Labels.txt"
+from Knn import KNN
+
+#  used to split the datasets into packs to satisfy the leave one out cross validation
+def cross_validation_split(dataset, folds=10):
+    index=0
+    dataset_split = []  # list holds data after separation
+    fold_size = int(len(dataset) / folds)
+    for i in range(folds):
+        fold = []
+        #appending the internal folds to the returned list
+        while len(fold) < fold_size:
+            fold.append(dataset[index])
+            index +=1   
+        dataset_split.append(fold)
+    return dataset_split
 
 
 
 
-# Split a dataset into k folds
-def cross_validation_split(dataset, folds=3):
-	dataset_split = list()
-	dataset_copy = list(dataset)
-	fold_size = int(len(dataset) / folds)
-	for i in range(folds):
-		fold = list()
-		while len(fold) < fold_size:
-			index = randrange(len(dataset_copy))
-			fold.append(dataset_copy.pop(index))
-		dataset_split.append(fold)
-	return dataset_split
-
-
-def data_load(train_path , test_path):
-    training_labels = open(train_path,"r") 
-    testing_labels = open(test_path,"r") 
-    training_label = training_labels.readlines()
+def data_load_with_LOOCV(train_path , test_path):
+    training_labels = open(train_path,"r")   #open the trian dataset folder
+    testing_labels  = open(test_path,"r" )   #open the test dataset folder
+    
+    # Reading the datasets
+    training_label = training_labels.readlines()   
     testing_label = testing_labels.readlines()
 
+    # Close the folders
     testing_labels.close()
     testing_labels.close()
 
-
-    Training_images = []
+    # two lists to holds the datasets
+    training_images = []
     testing_images = []
 
+    # Get the images one by one and fill the list and converting them to numpy array carry float number 
 
-    for i in range(1,2401,1):
-        train_img = np.array(Image.open("F:/Handwritten-digit-recognition/Task Dataset/Train/N"+str(i)+".jpg"))/255.0
-        Training_images.append(train_img)
+    # Training
+    for i in range(1,len(training_label)+1,1):
+        train_img = np.array(Image.open("../Task Dataset/Train/N"+str(i)+".jpg"))/255.0
+        training_images.append(train_img)
 
-
-    for j in range(1,201,1):
-        test_img = np.array(Image.open("F:/Handwritten-digit-recognition/Task Dataset/Test/N"+str(j)+".jpg"))/255.0
+    # Testing
+    for j in range(1,len(testing_label)+1,1):
+        test_img = np.array(Image.open("../Task Dataset/Test/N"+str(j)+".jpg"))/255.0
         testing_images.append(test_img) 
 
-
+    #covert training labels to int 
     edited_training_label = []
     for i in training_label :
         edited_training_label.append(int(i)) 
-    
+
+    #covert testing labels to int 
+    edited_testing_label = []
+    for i in testing_label :
+        edited_testing_label.append(int(i))     
    
-    Training_images,training_label = shuffle(Training_images, edited_training_label, random_state=0)
-    # Training_images = cross_validation_split(Training_images,10)
+    # shufflling the data and splits into 10 packs as the number of classes 
+    training_images,edited_training_label = shuffle(training_images, edited_training_label, random_state=0)
+    training_images = cross_validation_split(training_images,10)
+  
+   
+    return training_images ,testing_images ,edited_training_label, edited_testing_label
 
 
-    train_set = {0: [] ,1: [] ,2: [] ,3: [] ,4: [] ,5: [] ,6: [] ,7: [] ,8: [] ,9: []}
-    test_set = {0: [] ,1: [] ,2: [] ,3: [] ,4: [] ,5: [] ,6: [] ,7: [] ,8: [] ,9: []}
-
-    index = 0
-    for numbers in Training_images:
-        train_set[int(training_label[index])].append(numbers)  
-        index +=1
-
-    # index = 0
-    # for numbers in testing_images:
-    #     test_set[int(testing_label[index])].append(numbers)  
-    #     index +=1
-
-    return train_set , test_set , Training_images ,testing_images ,training_label, testing_label
-
-
-
-train_path =  "F:/Handwritten-digit-recognition/Task Dataset/Train/Training Labels.txt"
-test_path =  "F:/Handwritten-digit-recognition/Task Dataset/Test/test Labels.txt"
-
-train_set , test_set , Training_images ,testing_images ,training_label, testing_label = data_load(train_path,test_path)
-
-predect= KNN2(train_set,testing_images[1],k=11)
-print(predect , testing_label[1])
-# print("Labels :",training_label)
-
-# correct=0
-# total = 0
-# for i in range(200):
-#     predect= KNN(train_set,testing_images[i],k=11)
-#     if predect == int(testing_label[i]):
-#         correct +=1    
-#     total +=1
-#     print(predect,testing_label[i])
-
-
-# print((correct/total))
-
-# euclidian_dis = np.linalg.norm(np.array(Training_images[0]) - np.array(Training_images[0])) 
-# print(euclidian_dis)
-
-# for fold in Training_images : 
-#     for features in fold :
-#         euclidian_dis = np.linalg.norm(np.array(Training_images[0]) - np.array(Training_images[0])) 
-
-# predect,index = KNN(Training_images,training_label,testing_images[0],k=23)
-# print("Predecit : ",predect , testing_label[0])
